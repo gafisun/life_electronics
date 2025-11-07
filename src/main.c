5ad9808bafafd0db5dc9c0ccb34df6b5d461ffa8
phyfon <jone2484@purdue.edu>
2025-11-07 08:02:55 -0500

    more progress

--- src/main.c
+++ src/main.c
@@ -3,6 +3,9 @@
 #include "hardware/gpio.h"
 #include "pico/multicore.h"
 
+#define SIZE 32
+const char keymap[16] = "DCBA#9630852*741";
+int col;
 //definitions
 /*
 void init_keypad();
@@ -12,61 +15,72 @@ void init_matrix();
 */ // something like these are needed
 
 bool intr_flag;
-bool matrix_is_readable;
-bool matrix_is_writable;
+bool matrix_is_readable = true;
+bool matrix_is_writable = true;
+
 
 
 void init_keypad(){
     
-    //set 2-5 as outputs (so we can drive them)
-    for(int i = 2; i <=5; i++)
+    //set 6-9 as outputs (so we can drive them)
+    for(int i = 6; i <=9; i++)
     {
         gpio_set_dir(i,true);
         gpio_put(i,false);
         gpio_set_function(i,GPIO_FUNC_SIO);
     }
     
-    //set 6-9 as inputs (so we can read them)
-    for(int i = 6; i <= 9; i++){
+    //set 2-5 as inputs (so we can read them)
+    for(int i = 2; i <= 5; i++){
         gpio_init(i);
     }
 
 }
 
-
+    // when interrupted, we set the intr_flag to be true
 void stop_isr(){
-    gpio_acknowledge_irq(6,GPIO_IRQ_EDGE_RISE);
+    gpio_acknowledge_irq(2,GPIO_IRQ_EDGE_RISE);
     intr_flag = true;
 }
 
 
-//set up the code to wait for rising edge
+//set up the code to wait for rising edge on pin 2 (key D)
 void init_stop_isr(){
-    gpio_add_raw_irq_handler(6,stop_isr);
-    gpio_set_irq_enabled(6,GPIO_IRQ_EDGE_RISE,true);
+    gpio_add_raw_irq_handler(2,stop_isr);
+    gpio_set_irq_enabled(2,GPIO_IRQ_EDGE_RISE,true);
     irq_set_enabled(IO_IRQ_BANK0,true);
+    gpio_put(2,true);
 
 }
 
 void deinit_stop_isr(){
-    //
-    gpio_set_irq_enabled(6,GPIO_IRQ_EDGE_RISE,false);
+
+    gpio_set_irq_enabled(2,GPIO_IRQ_EDGE_RISE,false);
+    gpio_put(2,false);
 }
 
-/*
-void update_display(){
 
-    while(!read_mat_enable){
+/*
+void display_matrix(){
+
+    //waits until core 0 is done writing into the matrix
+    while(!matrix_is_readable){
         tight_loop_contents();
-    }
+    } 
+
+    //tells core 1 not to write to the matrix
     write_mat_enable = false;
 
-    for(int row = 0; row < 32; row++){
-        for(int col = 0; col < 32; col++){
+    //drive the LEDS 
+    for(int row = 0; row < SIZE; row++){
+        for(int col = 0; col < SIZE; col++){
             drive_led(row,col,matrix[row][col]);
         }
     }
+    //tells core 1 it can write again
+    write_mat_enable = true;
 
+    //wait until required based on LED matrix specs
     sleep_ms(drive_time_ms);
 }*/
 
@@ -77,18 +91,31 @@ int main(){
     // call inits
     /*
     init_keypad();
+
+    char key = 0;
+
+    multicore_launch_core1(display_matrix);
     
     while(1){
 
 
-    multicore_launch_core1(update_display);
+    
 
     //editor
-    do{
+    do
+    {
 
         //read key
-
+        key = read_key_blocking();
         //set cursor indication
+        if(key == '2'){
+        
+        }
+        else if(key == '4')
+        {
+
+        }
+        else if(key == '5')
 
 
         //perform instruction
@@ -105,9 +132,13 @@ int main(){
 
 
     } while(!intr_flag)
+    deinit_stop_isr();
 
 
     }
 
     */
+
+    for(;;){} //maintain program control indefinitely
+    return 0;
 }
